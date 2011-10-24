b9a247e0ef50b8d2ceb0a130d5d6c9cfc5764e1b
Becker <becker@gist.com>
2011-10-24 16:14:21 -0700

    html failwhale

---
+++ plugins/fail.rb
@@ -0,0 +1,38 @@
+
+$bot.add_command(
+	:syntax      => 'fail',
+	:description => 'Produces a whale',
+	:regex       => /^fail$/,
+	:is_public   => true
+) {
+'
+ <body>
+
+       \"""/ 
+        | |
+▄██████████████▄▐█▄▄▄▄█▌
+████████████████▌▀▀██▀▀
+████▄████████████▄▄█▌
+▄▄▄▄▄██████████████▀
+
+</body>
+  <html xmlns="http://jabber.org/protocol/xhtml-im">
+    <body xmlns="http://www.w3.org/1999/xhtml">
+      <p style="font-family:Andale Mono">
+
+       \"""/ 
+        | |
+▄██████████████▄▐█▄▄▄▄█▌
+████████████████▌▀▀██▀▀
+████▄████████████▄▄█▌
+▄▄▄▄▄██████████████▀
+
+      </p>
+    </body>
+  </html>
+
+        
+'
+}
+
+
