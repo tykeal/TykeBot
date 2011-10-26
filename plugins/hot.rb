7aba7fbd4ef06ddb14d70bd6895aeab7c52e9da9
Matt Hartzler <matt.hartzler@gmail.com>
2011-10-26 15:31:40 -0500

    add random popular tweet from trending topics

--- plugins/hot.rb
+++ plugins/hot.rb
@@ -12,6 +12,14 @@ plugin.add_command(
     p=REXML::Element.new('p')
     p.add_text("Here's what the kids are twatting about: ")
     trends=TwitterApi.new(plugin.config[:woeid]).hot.reject{|t| t["promoted_content"]}
+    tweet = TwitterApi.new.search(trends[rand(trends.size)]["name"],1).first
+    if tweet
+      p.add_text("@")
+      p.add_text(tweet["from_user"])
+      p.add_text(" ")
+      p.add_text(tweet["text"])
+      p.add_element('br')
+    end
     trends.each_with_index do |t,i|  
       a=p.add_element('a')
       a.text=t["name"]
@@ -28,10 +36,15 @@ end
 class TwitterApi
   DEFAULT_WOEID=2490383 # seattle
   URL='http://api.twitter.com/1/trends/%s.json'
-  def initialize(woeid)
+  SEARCH_URL='http://search.twitter.com/search.json?q=%s&lang=en&result_type=popular&rpp=%d'
+  def initialize(woeid=nil)
     @woeid=woeid||DEFAULT_WOEID
   end
   def hot
     JSON.parse(http_get(URL % @woeid).body).first["trends"]
   end
+  def search(q,limit)
+    JSON.parse(http_get(SEARCH_URL % [CGI.escape(q),limit]).body)["results"]
+  end
+
 end
