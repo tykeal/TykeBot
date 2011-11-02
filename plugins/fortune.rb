eeb1fbf829b48289937105976649349511402316
Andrew Grimberg <tykeal@bardicgrove.org>
2011-11-01 17:02:40 -0700

    Fortune welcome handler
    Hook the welcome handler up to our fortune mod

--- plugins/fortune.rb
+++ plugins/fortune.rb
@@ -7,3 +7,11 @@ end
 command(:fortune,
   :description => 'get a fortune'
 ) { get_fortune() }
+
+subscribe :give_fortune do |options|
+  fortune = get_fortune()
+  if !options[:fortune_prefix].nil?
+    fortune = options[:fortune_prefix] + fortune
+  end
+  bot.send(:text=>fortune)
+end
--- plugins/welcome.rb
+++ plugins/welcome.rb
@@ -1,2 +1,11 @@
-# this is annoying, lets skip for now
-#bot.welcome{ |person| "Hello #{person}!" }
+welcome_started = false
+
+bot.timer.add_timer(:timestamp=>Time.now + 2, :requestor=>'welcome_plugin') { welcome_started = true }
+
+bot.welcome do |person|
+  if welcome_started
+    publish(:give_fortune,{:fortune_prefix=>"Welcome #{person}! Here is a fortune for you:\n"})
+  end
+  # Return nil because the handler expects a text return to send to the room
+  nil
+end
