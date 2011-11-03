92e9ecb96606b80349223be00267c30b7e34a268
Matt Hartzler <matt.hartzler@gmail.com>
2011-11-02 23:13:33 -0500

    links plugin, makes use of the firehose

--- lib/utils.rb
+++ lib/utils.rb
@@ -68,3 +68,32 @@ end
 class Array
   def sample ; self[rand(size)] ; end
 end
+
+class File
+  # reverse_readline
+  def reverse_readline
+    buffer = 1024
+    lines = []
+    done = false
+    seek(0,SEEK_END)
+    while !done
+      if pos > buffer
+         to_read = buffer
+         seek(-to_read,SEEK_CUR)
+      else
+         to_read = pos
+         seek(0,SEEK_SET)
+         done=true
+      end
+      chunk = read(to_read)
+      chunk += lines.first unless lines.empty?
+      lines = chunk.split("\n")
+      lines[(done ? 0 : 1)..-1].reverse.each do |line| 
+        result = yield line
+        break unless result 
+      end
+      seek(-to_read,SEEK_CUR)
+    end
+  end
+end
+
---
+++ plugins/links.rb
@@ -0,0 +1,32 @@
+require 'uri'
+file=data_file('links.json')
+format=config[:date_format]||'%Y-%d-%m %I:%M%p'
+limit=(config[:limit]||20).to_i
+
+command(:links,
+  :optional=>[:n,:sender],
+  :description=>'show links from the room',
+  :html=>true
+) do |message,n,sender|
+  n = (n || 5).to_i
+  n = limit if n > limit
+  lines = []
+  open(file) do |f| 
+    f.reverse_readline do |line| 
+      info = JSON.parse(line) 
+      lines << info if !sender || info['from'].strip == sender.strip
+      lines.size<n 
+    end
+  end if File.exist?(file)
+  lines.reverse.map{|info| '%s %s <a href="%s">%s</a>' % [Time.at(info['time']).strftime(format),info['from'],info['url'],URI.parse(info['url']).host]}.join("\n<br/>")
+end
+
+subscribe(:firehose) do |bot,message|
+  URI.extract(message.body, ['http', 'https']).each do |url|
+    open(file,"a"){|f| f.puts JSON.generate({
+      :url=>url,
+      :from=>bot.sender(message),
+      :time=>Time.now.to_i
+    })}
+  end unless bot.sender(message) == bot.config[:name]
+end
