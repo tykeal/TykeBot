97bdf8e0d1e430e874d87ca7e2df412780c40ebd
Alan Shield <superhet@gmail.com>
2011-11-08 22:10:34 -0800

    Hartzler fixed up the bot

--- plugins/xkcd.rb
+++ plugins/xkcd.rb
@@ -1,19 +1,17 @@
 require 'json'
 
-QUERY_URL = "http://api.duckduckgo.com/?q=!ducky+xkcd+%s&format=json&no_redirect=1"
+QUERY_URL = "http://api.duckduckgo.com/?q=!ducky+site:www.xkcd.com+%s&format=json&no_redirect=1"
 
 def search(q)
-  q = q.gsub(" ", "+")
-  output = http_get(QUERY_URL % q).body
+  output = http_get(QUERY_URL % CGI.escape(q)).body
   JSON.parse(output)["Redirect"].first
 end
 
 command(:xkcd, 
-  :required=>:q
+  :required=>:q,
   :description => "Find an XKCD strip for a subject",
   :html => true
-)
-do |q|
-    search_output = search(q)
-	'<a href="%s">%s</a>' % [search_output, search_output]
+) do |message,q|
+  search_output = search(q)
+  '<a href="%s">%s</a>' % [search_output, search_output]
 end
