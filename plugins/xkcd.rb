ab3391c6ae560ad2873a11b7ebc1be2c3670e79e
iospera <iospera@yahoo.com>
2011-11-10 12:21:12 -0800

    make format_url work

--- plugins/xkcd.rb
+++ plugins/xkcd.rb
@@ -2,14 +2,14 @@ require 'json'
 
 QUERY_URL = "http://api.duckduckgo.com/?q=!ducky+site:m.xkcd.com+%s&format=json&no_redirect=1"
 
-def xkcd_format_url(url)
+def format_url(url)
   '<a href="%s">%s</a>' % [url, url]
 end
 
 def search(q)
   output = http_get(QUERY_URL % CGI.escape(q)).body
   url = JSON.parse(output)["Redirect"].first
-  /http[s]*:\/\/m.xkcd.com\/[0-9]+/.match(url) ? format(url) : "xkcd hasn't covered that subject. Are you sure you exist?"
+  /http[s]*:\/\/m.xkcd.com\/[0-9]+/.match(url) ? format_url(url) : "xkcd hasn't covered that subject. Are you sure you exist?"
 end
 
 command(:xkcd, 
