acf135f2918a37a5a8c45957c5b443dd03eb386e
Becker <becker@gist.com>
2011-11-11 15:23:01 -0800

    use xkcd's custom search

--- plugins/xkcd.rb
+++ plugins/xkcd.rb
@@ -8,9 +8,9 @@ def format_url(url,text=nil)
 end
 
 def search(q)
-  doc = Nokogiri::HTML(open('http://www.google.com/search?q=site:m.xkcd.com+'+CGI.escape(q)))
-  urls = doc.xpath('//cite').map{ |link| link.text}.select{|u| u.match(/m\.xkcd\.com\/\d+/)}.uniq
-  urls.first ? urls.map{|url| format_url("http://"+url)}.join("<br/>\n") : "xkcd hasn't covered that subject. Are you sure you exist?"
+  doc = Nokogiri::HTML(open( "http://www.google.com/cse?cx=012652707207066138651%3Azudjtuwe28q&ie=UTF-8&q=#{CGI.escape(q)}&sa=Search&siteurl=xkcd.com%2F#gsc.tab=0&gsc.q=dead%20pixel&gsc.page=1"))
+  urls = doc.xpath('//a').map{ |link| link.attributes["href"].to_s}.select{|u| u.match(/xkcd\.com\/\d+/)}.uniq
+  urls.first ? urls.map{|url| format_url(url)}.join("<br/>\n") : "xkcd hasn't covered that subject. Are you sure you exist?"
 end
 
 command(:xkcd, 
