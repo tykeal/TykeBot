9c88f644a94e03f6298ab2f84f86458290a52f32
Becker <becker@gist.com>
2011-11-11 15:50:49 -0800

    feaking google

--- plugins/xkcd.rb
+++ plugins/xkcd.rb
@@ -8,9 +8,10 @@ def format_url(url,text=nil)
 end
 
 def search(q)
-  doc = Nokogiri::HTML(open( "http://www.google.com/cse?cx=012652707207066138651%3Azudjtuwe28q&ie=UTF-8&q=#{CGI.escape(q)}&sa=Search&siteurl=xkcd.com%2F#gsc.tab=0&gsc.q=dead%20pixel&gsc.page=1"))
+ 
+  doc = Nokogiri::HTML(open("http://www.google.com/cse?cx=012652707207066138651:zudjtuwe28q&ie=UTF-8&q=#{CGI.escape(q)}&sa=Search&siteurl=xkcd.com/&nojs=1"))
   urls = doc.xpath('//a').map{ |link| link.attributes["href"].to_s}.select{|u| u.match(/xkcd\.com\/\d+/)}.uniq
-  urls.first ? urls.map{|url| format_url(url)}.join("<br/>\n") : "xkcd hasn't covered that subject. Are you sure you exist?"
+  urls.first ? urls.map{|url| format_url(url)}.join("\n<br/>") : "xkcd hasn't covered that subject. Are you sure you exist?"
 end
 
 command(:xkcd, 
