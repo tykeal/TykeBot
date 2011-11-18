a2f55471c037b139015694b73ee8a5b29ad56236
Andrew Grimberg <tykeal@bardicgrove.org>
2011-11-17 19:36:18 -0800

    Pull the who check and just stick it in the update notice for the moment

--- plugins/update.rb
+++ plugins/update.rb
@@ -11,8 +11,8 @@ add_command(
   :description => 'Internal command for updating based upon GitHub updates',
   :regex => /^\[TykeBot\] (\w+) pushed \d+ new commits to master:.+$/
 ) do |message,who|
-  if (bot.sender(message) == 'github-services@jabber.org' && who != 'none')
-    bot.send(:text=>'A checkin on GitHub has initiated a bot update, one moment please...')
+  if bot.sender(message) == 'github-services@jabber.org'
+    bot.send(:text=>"A checkin on GitHub has initiated a bot update by #{who}, one moment please...")
     updatescript
   end
 end
