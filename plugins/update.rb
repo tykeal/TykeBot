4a4ee3157af84bd8385464484d692023a93249d8
Andrew Grimberg <tykeal@bardicgrove.org>
2011-11-17 19:30:19 -0800

    First real try of having GitHub cause the bot to do an update

--- plugins/update.rb
+++ plugins/update.rb
@@ -1,8 +1,28 @@
-#command(:rand, :description => 'Produce a random number from 0 to 10') { rand(10).to_s }
+def updatescript
+  `#{config[:update_script]}`
+end
 
 command(:update, :description => 'Make bot update to the latest revision', :is_public => false) {
-  `#{config[:update_script]}`
+  updatescript
 }
+
+add_command(
+  :syntax => 'GitUpdateHandler',
+  :description => 'Internal command for updating based upon GitHub updates',
+  :regex => /^\[TykeBot\] (\w+) pushed \d+ new commits to master:.+$/
+) do |message,who|
+  if (bot.sender(message) == 'github-services@jabber.org' && who != 'none')
+    bot.send(:text=>'A checkin on GitHub has initiated a bot update, one moment please...')
+    updatescript
+  end
+end
+
+on :firehose do |bot,message|
+#  if bot.sender(message) == 'github-services@jabber.org'
+#    puts message
+#  end
+  puts "received stanza:\n" + bot.sender(message)
+end
 
 init do
   config[:update_script] ||= '~/deploy/tykebot/current/scripts/run_update.sh'
