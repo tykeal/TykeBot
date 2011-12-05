adeca526744858404d0a9010e021423b974f983d
Becker <becker@gist.com>
2011-12-05 13:03:55 -0800

    Added private chat wootoff watcher

--- plugins/woot.rb
+++ plugins/woot.rb
@@ -2,53 +2,43 @@ require 'json'
 require 'nokogiri'
 require 'open-uri'
 require "cgi"
-last_check= Time.now
 last_woot = nil
-is_woot_off=false
+is_woot_off = false
+woot_watchers = []
+
 def woot()
   output =Nokogiri::HTML( http_get('http://www.woot.com/').body)
   is_woot_off = output.search("img").select{|i| i.attributes["src"].value =~ /woot-off/}.size > 0
-  last_check=Time.now
   title = output.search("div.productDescription").search("h2").text
   price = output.search("div.productDescription").search("h3").text
-  link = output.search("div.productDescription").search("h5 a")[0].attributes["href"].value
+  link = output.search("div.productDescription").search("h5 a")[0].attributes["href"] ? output.search("div.productDescription").search("h5 a")[0].attributes["href"].value : "sold out!"
   last_woot = title+" "+price+"\n"+link
 end
 
 command(:woot,:description => 'Get the current woot item') { woot }
 
-=begin
-
-on :join do |bot|
-  timer(2) { started = true }
-end
-
-on :welcome do |bot,message|
-  if (started && rand(config[:random]) < 1)
-    send(:text => fortune("Welcome %s! Here have a fortune:\n" % bot.sender(message)))
+command(:wootoff,:required=>[:state],:description => 'For private chat. start/stop posting the woot off!') {|message,state| 
+  if !message.group_chat? && state.strip!= "list"
+    sender = message.from.to_s.split("/").first
+    state.strip=="start" ? woot_watchers.push(sender)  :  woot_watchers.delete(sender) 
+    woot_watchers.uniq!
+    save_data(woot_watchers)
+    "You have been #{state.strip=="start" ? 'added to':'removed from'} the watch list"
+  elsif(state.strip=="list")
+    woot_watchers.map(&:to_s).join("\n")
   end
-end
-
-on :firehose do |bot,message|
-  last_active = Time.now if started && bot.sender(message) != bot.config[:name]
-end
+}
 
 init do
-  # just give our random welcome fortune a small chance
-  # can be set in config yaml
-  config[:random] ||= 3
-
-  # number of seconds to wait between messages before spewing a fotune
-  config[:timer_push] ||= 5400 # 90 minutes
-  last_sent = 0
-
-  (check = Proc.new { 
-    if last_sent.to_i < last_active.to_i && Time.now-last_active >= config[:timer_push]
-      send :text=>fortune("It's been quiet too long.  I think we need a fortune to liven things up!\n\n") rescue error
-      last_sent = Time.now
+  woot_watchers=load_data||[]
+  (check = Proc.new {
+    past_woot=last_woot.to_s.clone
+    if  woot_watchers.size>0
+      last_woot =  woot() 
+      if(past_woot!=last_woot)
+        send :to=>woot_watchers.uniq, :text=>("New Woot!\n"+last_woot) rescue error
+      end
     end
-    timer(600, &check)
-  }).call
+    timer(config[:woot_off_check]||30,&check)
+  }).call()
 end
-
-=end
