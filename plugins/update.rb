088879d3b1e7729901f94e6bbe920448e8857e0d
Andrew Grimberg <tykeal@bardicgrove.org>
2011-12-13 07:13:29 -0800

    Make update plugin drink from the firehose
    This change has the update plugind drinking from the firehose so that we
    can make the ugly [TykeBot] alias go away.

--- plugins/update.rb
+++ plugins/update.rb
@@ -2,7 +2,7 @@ config :update_script, :default=>'~/deploy/tykebot/current/scripts/run_update.sh
 config :github_jid, :default=>'github-services@jabber.org', :description=>'jabber id of the github xmpp hook'
 
 command do
-  aliases '[TykeBot]' # a small hack to make this work...
+#  aliases '[TykeBot]' # a small hack to make this work...
   description 'Make bot update to the latest revision'
 
   action :is_public=>false do |message|
@@ -10,15 +10,25 @@ command do
     timer(3){updatescript}
   end
 
-  action :required=>:details, :description => 'Internal action for updating based upon GitHub XMPP updates.' do |message,details|
-    info = details.match(/^(\w+) pushed (\d+) new commits to master:.+$/)
+#  action :required=>:details, :description => 'Internal action for updating based upon GitHub XMPP updates.' do |message,details|
+#    info = details.match(/^(\w+) pushed (\d+) new commits to master:.+$/)
+#    if bot.sender(message) == config.github_jid && info
+#      # info[1] is who, info[2] is number of commits
+#      bot.send(:text=>"A checkin on GitHub by #{info[1]} has initiated a bot update.  One moment please...\n\n#{details}")
+#      # give it a few seconds to let the room know...
+#      timer(3){updatescript}
+#    end
+#  end
+end
+
+on :firehose do |bot,message|
+    info = details.match(/^\[TykeBot\] (\w+) pushed (\d+) new commits to master:.+$/)
     if bot.sender(message) == config.github_jid && info
       # info[1] is who, info[2] is number of commits
       bot.send(:text=>"A checkin on GitHub by #{info[1]} has initiated a bot update.  One moment please...\n\n#{details}")
       # give it a few seconds to let the room know...
       timer(3){updatescript}
     end
-  end
 end
 
 helper :updatescript do
