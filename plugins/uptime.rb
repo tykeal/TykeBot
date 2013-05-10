1b4ae760e3fe85e79b78301071bdfb666bd81690
Matt Hartzler <matt.hartzler@gmail.com>
2013-05-10 10:41:24 -0500

    attempt to fix uptime regex

--- plugins/uptime.rb
+++ plugins/uptime.rb
@@ -2,7 +2,7 @@ upsince=Time.now
 command do
   description "Show current uptime for the bot"
   action do
-    captures = (`uptime`.match /up (?:(?:(\d+) days,)?\s+(\d+):(\d+)|(\d+) min)/).captures
+    captures = (`uptime`.match /up\s+(?:(\d+) days,)?\s*(?:(\d+):(\d+)|(?:(\d+) min))/).captures
     elapsed_seconds = captures.zip([86440, 3600, 60, 60]).inject(0) do |total, (x,y)|
       total + (x.nil? ? 0 : x.to_i * y)
     end
