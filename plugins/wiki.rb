ff3a31c082134edc6aa9bd8abcd500478632cdae
Andrew Grimberg <tykeal@bardicgrove.org>
2013-06-19 20:58:03 -0700

    Make wiki plugin a bit more useful
    Improve the wiki plugin to actually link out the articles. Additionally,
    the original query wasn't doing a proper limit. This is now fixed and we
    return emit the 5 hits the query returns.
    
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- plugins/wiki.rb
+++ plugins/wiki.rb
@@ -1,14 +1,14 @@
-config :url, :default=>"http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=%s&format=json&limit=5", :description=>'search url with %s for query string'
+config :url, :default=>"http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=%s&format=json&srlimit=5", :description=>'search url with %s for query string'
 
 command do
   description "Find wikipedia summaries for a subject"
 
   action :required=>:q, :html=>true do |message,q|
     if results = wiki_search(q)
-      response = results[0..0].map{|item|
-        "<li><strong>#{item["title"]}</strong> - #{item["snippet"]}</li>\n".
+      response = results.map{|item|
+        "<a href='http://en.wikipedia.org/wiki/#{URI.escape(item["title"])}'><strong>#{item["title"]}</strong></a> - #{item["snippet"]}><br/>".
           gsub("<span class='searchmatch'>","<em>").gsub("</span>", "</em>")}
-      "<ul>#{response}</ul>"
+      "#{response}"
     end
   end
 end
